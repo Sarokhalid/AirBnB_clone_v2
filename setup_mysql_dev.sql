d1ab64d8aa8bd2a1f13ad76c32a8ac74cb514496
sara <saramustafa1842002@gmail.com>
2024-02-18 07:17:02 -0800

    prepere mysql server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,10 @@
---prepares mysql server
---create database if not exists
+-- prepares mysql server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---create user if not exist
+-- create user if not exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---grant all privillages
+-- grant all privillages
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
---grant performance_schema
+-- grant performance_schema
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
---Flushing privileges
+-- Flushing privileges
 FLUSH PRIVILEGES;
