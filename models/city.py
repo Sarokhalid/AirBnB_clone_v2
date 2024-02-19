f02d0f7f6a6db147ef244dee3c6ab786eaf4807b
sara <saramustafa1842002@gmail.com>
2024-02-19 08:32:32 +0200

    update Amenity

--- models/city.py
+++ models/city.py
@@ -2,18 +2,18 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 import os
-from sqlalchemy import Column, String, Foreignkey
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tabename__ = 'cities'
     name = Column(
         String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     state_id = Column(
-        String(60), Foreignkey('state.id'), nullable=False
+        String(60), ForeignKey('state.id'), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     places = relationship(
         'Place',
--- models/state.py
+++ models/state.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import relationship
 from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(
