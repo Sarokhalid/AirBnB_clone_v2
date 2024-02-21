c0b09aee2ebab8a3604c7ac15439ba1d1b7a32c0
sara <saramustafa1842002@gmail.com>
2024-02-21 09:16:49 +0200

    done

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+""" City module for HBNB project """
 from models.base_model import BaseModel, Base
 import os
 from sqlalchemy import Column, String, ForeignKey
