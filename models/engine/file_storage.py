815c24157a772ffde2a8040166d14a6b16b6b3f4
sara <saramustafa1842002@gmail.com>
2024-02-21 11:59:28 +0200

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,9 +10,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def __init__(self):
+    def reload(self):
         """initialize filestorage instance"""
-        self.model_classes = {
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.city import City
+        from models.state import State
+        from models.review import Review
+        from amenity import Amenity
+        classes = {
             'BaseModel': import_module('models.base_model').BaseModel,
             'User': import_module('models.user').User,
             'State': import_module('models.state').State,
@@ -21,47 +28,46 @@ class FileStorage:
             'Place': import_module('models.place').Place,
             'Review': import_module('models.review').Review
         }
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    cls_name = val['__class__']
+                    if cls_name in classes:
+                        self.all()[key] = classes[cls_name](**val)
+        except FileNotFoundError:
+            pass
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is None:
-            return self.__objects
+            return FileStorage.__objects
         else:
-            flt_dict = {}
-            for key, value in self.__objects.items():
-                if type(value) is cls:
-                    flt_dict[key] = value
-            return flt_dict
+            objects_of_cls = {}
+            for key, obj in FileStorage.__objects.items():
+                if cls.__name__ == obj.__class__.__name__:
+                    objects_of_cls[key] = obj
+            return objects_of_cls
 
     def delete(self, obj=None):
         """Removes an object from the storage dictionary"""
-        if obj is not None:
-            obj_key = obj.to_dict()['__class__'] + '.' + obj.id
-            if obj_key in self.__objects.keys():
-                del self.__objects[obj_key]
+        if obj is None:
+            return
+        key = obj.__class__.__name__ + '.' + obj.id
+        if key in self.all():
+            del self.all()[key]
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.all()[obj.__class__.__name__ + '.' + obj.id] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
+            for key, val in FileStorage__objects.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
-
-    def reload(self):
-        """Loads storage dictionary from file"""
-        classes = self.model_classes
-        if os.path.isfile(self.__file_path):
-            temp = {}
-            with open(self.__file_path, 'r') as file:
-                temp = json.load(file)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
 
     def close(self):
         """closes storage engine"""
